2f4be0d77dd5c5ef0b0dc93ba942b35788e8d12c
shashiranjan001 <shashiranjan96kgp@gmail.com>
2023-08-11 15:24:41 +0900

    Fix find_number_of_array_diff_1/find_number_of_array_diff_1.cpp

--- dynamic_programming/classroom_questions/find_number_of_array_diff_1/find_number_of_array_diff_1.cpp
+++ dynamic_programming/classroom_questions/find_number_of_array_diff_1/find_number_of_array_diff_1.cpp
@@ -6,17 +6,25 @@ signed main() {
     int n, m;
     std::cin >> n >> m;
     std::vector<std::vector<int> > dp(n+1, std::vector<int>(m+1, 0));
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= m; i++) {
+        dp[1][i] = 1;
+    }
+    for (int i = 2; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
             dp[i][j] = dp[i-1][j];
             if (j-1 >= 1) dp[i][j] += dp[i-1][j-1];
             if (j+1 <= m) dp[i][j] += dp[i-1][j+1];
         }
     }
-    int ans = dp[n][1];
-    for (int j = 2; j <= m; j++) {
-        ans = std::max(ans, dp[n][j]); 
+
+    for (int len = 1; len <= n; len++) {
+        for (int j = 1; j <= m; j++) std::cout << dp[len][j] << " ";
+        std::cout << std::endl;
+    }
+    int ans = 0;
+    for (int j = 1; j <= m; j++) {
+        ans += dp[n][j]; 
     }
     std::cout << ans << std::endl;
-    
+
 }
